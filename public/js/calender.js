2ba41098a9f5626969f2631865733161015c5dca
Jelle2003 <jelle.kerkhofs@outlook.com>
2024-11-19 11:34:43 +0100

    Update calender.js

--- public/js/calender.js
+++ public/js/calender.js
@@ -1,12 +1,21 @@
+const vandaag = new Date();
+const overEenWeek = new Date();
+const eenWeekGeleden = new Date();
+overEenWeek.setDate(vandaag.getDate() + 6); // Stel het einde van de week in (7 dagen vanaf vandaag)
+eenWeekGeleden.setDate(vandaag.getDate() - 6) // Stel een week gelden in (7 dagen vanaf vandaag)
+
+//dit nog in andere file steken
 // Initialiseer flatpickr voor een enkele datumselectie
 flatpickr("#singleDate", {
-    dateFormat: "Y-m-d", // Standaard datumnotatie
+    dateFormat: "d-m-Y", // Standaard datumnotatie
     allowInput: true, // Sta handmatige invoer toe
+    defaultDate: vandaag,
 });
 
 // Initialiseer flatpickr voor een datumreeksselectie
 flatpickr("#dateRange", {
     mode: "range", // Zet modus op 'range' voor begindatum en einddatum
-    dateFormat: "Y-m-d",
+    dateFormat: "d-m-Y",
     allowInput: true,
+    defaultDate: [eenWeekGeleden, vandaag],
 });
